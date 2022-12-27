5b3c401f52dc91eb5e123c89079d01a377e4b9e8
Sonja Sławek <sonjaslawek@gmail.com>
2022-12-27 13:54:37 +0100

    Added first view of app, making graphics and button.

---
+++ HowManyPizzas/HowManyPizzas/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HowManyPizzas
+//
+//  Created by Sonja on 22/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func StartButton(_ sender: Any) {
+        
+    }
+    
+}
+
