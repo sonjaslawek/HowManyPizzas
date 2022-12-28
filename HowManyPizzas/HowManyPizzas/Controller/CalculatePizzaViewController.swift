b1c5bdbcadc083a2a4a06513bdd336df09e4a0e8
Sonja Sławek <sonjaslawek@gmail.com>
2022-12-28 14:15:24 +0100

    Added connections for all elements to CalculatePizzaViewController

--- HowManyPizzas/HowManyPizzas/Controller/CalculatePizzaViewController.swift
+++ HowManyPizzas/HowManyPizzas/Controller/CalculatePizzaViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 class CalculatePizzaViewController: UIViewController {
     
     
+    @IBOutlet weak var PizzaSizeTextfield: UITextField!
+    
+    @IBOutlet weak var NumberOfPeopleTextField: UITextField!
+    
+    @IBOutlet weak var NumberOfSlices: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
   
+    @IBAction func CalculateButtonPressed(_ sender: Any) {
+    }
+    
+    
+    
 
 
 }
